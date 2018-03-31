e8d5dbfafb32c4c871913e913aa3503d1bd73ee3
Alex ter Weele <act85@cornell.edu>
2018-03-31 08:44:09 -0500

    Check spec assertions during dev.
    * project.clj: set JVM option clojure.spec.check-asserts during
      development and testing.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
-                 [org.clojure/math.combinatorics "0.1.4"]])
+                 [org.clojure/math.combinatorics "0.1.4"]]
+  :profiles {:dev {:jvm-opts ["-Dclojure.spec.check-asserts=true"]}})

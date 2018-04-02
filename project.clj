8f56bd0153381aeec270f05827911e4f381507e2
Alex ter Weele <act85@cornell.edu>
2018-04-02 09:51:05 -0500

    Fix test selectors.

--- project.clj
+++ project.clj
@@ -9,5 +9,5 @@
   :profiles {:dev {:jvm-opts ["-Dclojure.spec.check-asserts=true"]}}
   :plugins [[lein-marginalia "0.9.1"]]
   :marginalia {:javascript ["https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=TeX-MML-AM_CHTML"]}
-  :test-selectors {:default {complement :expensive}
+  :test-selectors {:default (complement :expensive)
                    :expensive :expensive})
